5ab08678534374dd9d1ea2576431287a195451d6
Your Name <pavlikkim200401@gmail.com>
2023-02-17 19:06:29 +0600

    grammar fixes

--- internal/transport/telegram/weather.go
+++ internal/transport/telegram/weather.go
@@ -12,16 +12,16 @@ import (
 func (h *Handler) getWeatherByCity(c telebot.Context) error {
 	city := c.Message().Payload
 	if city == "" {
-		return c.Send("Please specify a city name. E.g: //weather Astana")
+		return c.Send(`Пожалуйста, введите название города . E.g: /weather Astana`)
 	}
 	weather, err := h.service.Get(city, viper.GetString("weather_api"))
 	if err != nil {
 		h.lg.Errorln(err)
 		if errors.Is(err, domain.ErrCityNotFound) {
-			return c.Send(fmt.Sprintf("City %s not found", city))
+			return c.Send(fmt.Sprintf("Город %s не найден", city))
 		}
 		return err
 	}
-	return c.Send(fmt.Sprintf("В городе %s температура: %0.2f°C, ощущается как: %0.2f°C",
+	return c.Send(fmt.Sprintf("В городе %s, температура: %0.2f°C, ощущается как: %0.2f°C",
 		weather.Location.CityName, weather.Data.Values.Temperature, weather.Data.Values.TemperatureApparent))
 }
